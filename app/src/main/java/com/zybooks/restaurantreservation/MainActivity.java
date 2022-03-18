3d6e73e010a69b5671d239978f20a273f0f09b01
Walker-Ferencz1121 <wsferencz@gmail.com>
2022-03-18 16:53:42 -0400

    Help_Activity

--- app/src/main/java/com/zybooks/restaurantreservation/MainActivity.java
+++ app/src/main/java/com/zybooks/restaurantreservation/MainActivity.java
@@ -2,7 +2,9 @@ package com.zybooks.restaurantreservation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onHelpClick(View view) {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
+
 }
