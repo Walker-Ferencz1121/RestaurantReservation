5d4cc7e13dbc51633f02c99667e7cc5e29f86edc
SubZeRoy9 <turnerjroy@gmail.com>
2022-04-14 18:42:34 -0400

    4-14-22 #3

--- app/src/main/java/com/zybooks/restaurantreservation/StartingActivity.java
+++ app/src/main/java/com/zybooks/restaurantreservation/StartingActivity.java
@@ -6,22 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class StartingActivity extends AppCompatActivity {
 
-
+    //create buttons
     Button floor1;
     Button floor2;
+    Button patio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
 
+        //initializes buttons with restaurant layout activities
         floor1 = (Button) findViewById(R.id.f1);
         floor2 = (Button) findViewById(R.id.f2);
+        patio = (Button) findViewById(R.id.p1);
 
+        //makes floor 1 button go to MainActivity
         floor1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,10 +32,19 @@ public class StartingActivity extends AppCompatActivity {
             }
         });
 
+        //makes floor2Activity button go to floor2Activity activity
         floor2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(StartingActivity.this, floor2.class));
+                startActivity(new Intent(StartingActivity.this, floor2Activity.class));
+            }
+        });
+
+        //makes patioActivity button go to patio activity
+        patio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartingActivity.this, patioActivity.class));
             }
         });
 
